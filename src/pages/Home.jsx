10ec044245f1b3987e36bad418f9ade23dda1b2e
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-09-06 15:31:45 +0300

    addedHome component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,18 @@
 import React from "react";
-import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
-    <div>
       <div className="bg-primary w-full overflow-hidden">
         <div className={`sm:px-16 px-6 flex justify-center items-center`}>
           <div className={`xl:max-w-[1280px] w-full`}>
-            <Navbar />
+            <div className="mt-3">
+              <Hero/>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    
   );
 };
 
