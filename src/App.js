ea7d8b2f73c032b6ddc04ecbec19b5d14daf6de6
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-09-06 15:34:20 +0300

    added routes to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="app">
-      <Home/>
+      <Navbar />
+      <hr/>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
