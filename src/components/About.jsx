f2aaaa1785087b5439cb373b8401a5c6b788216e
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-09-06 15:33:29 +0300

    added about page

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`sm:px-16 px-6 flex justify-center items-center`}>
+        <div className={`xl:max-w-[1280px] w-full`}>
+          <div className="mt-3">About</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
