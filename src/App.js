edaff9f66b154da37f6c6e149d06c0b0e7974fd5
ELIUS WANYORO <109586789+EliusCaleb@users.noreply.github.com>
2023-09-11 09:23:19 +0300

    setting the app to receive data from backend

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+//import StudentSidebar from "./student/StudentSidebar";
+//import TeacherSidebar from "./teacher/TeacherSidebar";
 function App() {
   return (
     <div className="app">
-      <Navbar />
+       <Navbar />
       <hr/>
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-      </Routes>
+      </Routes> 
+      {/* <TeacherSidebar/> */}
+      {/* <StudentSidebar/> */}
+      
+
+      
+      
+
+      
+      
     </div>
   );
 }
